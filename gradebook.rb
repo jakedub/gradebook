2e0c231c5f02e637508427bfe7eeb5538efa4e46
jakedub <jacob.w.moore@gmail.com>
2017-09-13 11:24:58 -0400

    original commit

---
+++ gradebook.rb
@@ -0,0 +1,69 @@
+require 'csv'
+
+def print_gradebook(filename)
+ grade_book = CSV.read("input1.csv", headers:true).each do |grade|
+   grade_book.each do |row|
+     student.drop(0)
+     grade.drop(1)
+     grade_hash[student] = grade
+   end
+
+end
+
+def apply_average (all)
+  all.sum/all.size
+end
+
+def average(grade_hash)
+  new_hash = {}
+  grade_hash.each do |key,value|
+    new_hash[key] = (value.reduce{|sum, num|} sum + num}/ value.length)
+  end
+  new_hash
+end
+
+def assignment_scores(grade_hash, assignment_num)
+    as = grade_hash.map{|key, value| grade_hash[key] [assignment_num -1]}
+
+def assignment_scores_average(grade_hash, assignment_num)
+    as = grade_hash.map{|key, value| grade_hash[key] [assignment_num -1]}
+    apply_average(as)
+end
+
+def assignment_average (grade_hash, assignment_num)
+  (assignment_scores(grade_hash, assignment_num).reduce(:+)/grade_arr.length)
+end
+
+def class_average(grade_hash)
+  grades = grade_hash.values.flatten
+  apply_average(grades)
+end
+
+def letter(score)
+  if score >= 90
+    "A"
+  elsif score >= 80
+    "B"
+  elsif score >= 70
+    "C"
+  elsif score >= 60
+    "D"
+  else
+    "F"
+  end
+end
+
+
+if __FILE__ == $0
+  filename = $ARGV[0]
+  if filename
+    full_filename = File.absolute_path(filename)
+    if File.exists?(full_filename)
+      print_gradebook(full_filename)
+    else
+      puts "#{filename} does not exist!"
+    end
+  else
+    puts "Please give a filename as an argument."
+  end
+end
